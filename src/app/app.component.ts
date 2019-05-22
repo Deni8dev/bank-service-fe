959f42f3d28554a02e81238e15291cacca09cebb
Denilson Anachury <danachury@easysol.net>
2019-05-22 17:03:24 -0500

    Added pug, sass and page icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.pug',
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'bank-service-fe';
+  title = 'Salamandra Bank';
 }
