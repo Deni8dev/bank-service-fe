9a82517386ea8fb0cb6f8a1a5c7fb5f0fa57f95f
aduquino <aduquino@easysol.net>
2019-05-27 08:24:38 -0500

    Add routing components

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,12 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  profileGroup: FormGroup;
+  accountGroup: FormGroup;
   userName = 'Salamandra';
 
   constructor() {
   }
 
   ngOnInit() {
+    this.buildForm();
+  }
+
+  edit() {
+    console.error('Login in ' + this.profileGroup.getRawValue());
+  }
+
+  private buildForm() {
+    this.profileGroup = new FormGroup({
+      username: new FormControl('', Validators.required),
+    });
+    this.accountGroup = new FormGroup({
+      account: new FormControl('', Validators.required),
+    });
   }
 
 }
