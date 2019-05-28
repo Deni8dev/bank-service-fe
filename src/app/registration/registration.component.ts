cf0a74431a0cdfdf9d8589e4401d205f17d0ae26
aduquino <aduquino@easysol.net>
2019-05-28 15:38:56 -0500

    Update styles, registration component

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,23 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-export interface Gender {
-  value: string;
-  viewValue: string;
-}
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.pug',
   styleUrls: ['./registration.component.sass']
 })
 export class RegistrationComponent implements OnInit {
+
   registrationGroup: FormGroup;
   title = 'Salamandra Bank';
-  genders: Gender[] = [
-    {value: 'Male-0', viewValue: 'Male'},
-    {value: 'female-1', viewValue: 'Female'},
-    {value: 'other-3', viewValue: 'Other'}
-  ];
+
   constructor() {
   }
 
@@ -25,17 +19,25 @@ export class RegistrationComponent implements OnInit {
     this.buildForm();
   }
 
-  private buildForm() {
-    this.registrationGroup = new FormGroup({
-      typeMovement: new FormControl('', Validators.required),
-      valueMovement: new FormControl('', Validators.required),
-      dateMovement: new FormControl('', Validators.required),
-      tpAccount: new FormControl('', Validators.required),
-      tpIdentification: new FormControl('', Validators.required),
-      tpName: new FormControl('', Validators.required)
-    });
-  }
   Registration() {
     console.error('Login in ' + this.registrationGroup.getRawValue());
   }
+
+  private buildForm() {
+    this.registrationGroup = new FormGroup({
+      name: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      identificationType: new FormControl('', Validators.required),
+      identification: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      country: new FormControl('', Validators.required),
+      gender: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      passwordConfirmation: new FormControl('', Validators.required)
+    });
+  }
+
 }
