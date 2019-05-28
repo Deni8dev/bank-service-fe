f41afa980c3aba9a31ffc274d4b593e5c644e6b8
Camilo Carrillo <camilo.carrillo@cyxtera.com>
2019-05-28 13:43:08 -0500

    Register button added Register form added

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,23 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
-
+export interface Gender {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.pug',
   styleUrls: ['./registration.component.sass']
 })
 export class RegistrationComponent implements OnInit {
-
   registrationGroup: FormGroup;
   title = 'Salamandra Bank';
-
+  genders: Gender[] = [
+    {value: 'Male-0', viewValue: 'Male'},
+    {value: 'female-1', viewValue: 'Female'},
+    {value: 'other-3', viewValue: 'Other'}
+  ];
   constructor() {
   }
 
   ngOnInit() {
+    this.buildForm();
   }
 
+  private buildForm() {
+    this.registrationGroup = new FormGroup({
+      typeMovement: new FormControl('', Validators.required),
+      valueMovement: new FormControl('', Validators.required),
+      dateMovement: new FormControl('', Validators.required),
+      tpAccount: new FormControl('', Validators.required),
+      tpIdentification: new FormControl('', Validators.required),
+      tpName: new FormControl('', Validators.required)
+    });
+  }
   Registration() {
     console.error('Login in ' + this.registrationGroup.getRawValue());
   }
